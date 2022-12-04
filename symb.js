d32be64e31aede9664ca4884220823717a68930b
hajarham <118139172+hajarham@users.noreply.github.com>
2022-12-04 16:00:39 +0100

    Add files via upload

---
+++ symb.js
@@ -0,0 +1,19 @@
+var symb = {
+    "type": "FeatureCollection",
+    "name": "symb",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "CODE": 12.0, "REGIONS": "Ed-Dakhla - Oued Ed Dahab", "lon" : -14.607255118093928 , "lat" : 22.934157667828075 , "HOTSPOT": 0 }, "geometry": { "type": "Point", "coordinates": [ -14.607255118093928, 22.934157667828075 ] } },
+    { "type": "Feature", "properties": { "CODE": 11.0, "REGIONS": "Laayoune - Saguia Al Hamra", "lon" : -11.49753370874619 , "lat" : 26.331383193883294 , "HOTSPOT": 0 }, "geometry": { "type": "Point", "coordinates": [ -11.49753370874619, 26.331383193883294 ] } },
+    { "type": "Feature", "properties": { "CODE": 10.0, "REGIONS": "Gouelmim - Oued Noun", "lon" : -10.095136204683342 , "lat" : 29.781168051554445 , "HOTSPOT": 1 }, "geometry": { "type": "Point", "coordinates": [ -10.095136204683342, 28.298817013661584 ] } },
+    { "type": "Feature", "properties": { "CODE": 9.0, "REGIONS": "Sous - Massa", "lon" : -8.183772146613638 , "lat" : 28.298817013661584 , "HOTSPOT": 25 }, "geometry": { "type": "Point", "coordinates": [ -8.183772146613638, 29.781168051554445 ] } },
+    { "type": "Feature", "properties": { "CODE": 8.0, "REGIONS": "Daraa - Tafilalt", "lon" : -5.699999161592401 , "lat" : 31.210430958586343 , "HOTSPOT": 8 }, "geometry": { "type": "Point", "coordinates": [ -5.699999161592401, 31.210430958586343 ] } },
+    { "type": "Feature", "properties": { "CODE": 2.0, "REGIONS": "Oriental", "lon" : -2.412736568538467 , "lat" : 33.566603140925551 , "HOTSPOT": 16 }, "geometry": { "type": "Point", "coordinates": [ -2.412736568538467, 33.566603140925551 ] } },
+    { "type": "Feature", "properties": { "CODE": 1.0, "REGIONS": "Tanger - Tétouan - Al Houciema", "lon" : -5.430825796559335 , "lat" : 35.21883135300574 , "HOTSPOT": 141 }, "geometry": { "type": "Point", "coordinates": [ -5.430825796559335, 35.21883135300574 ] } },
+    { "type": "Feature", "properties": { "CODE": 4.0, "REGIONS": "Rabat - Sallé - Kénitra", "lon" : -6.31345772607836 , "lat" : 34.091198833058371 , "HOTSPOT": 46 }, "geometry": { "type": "Point", "coordinates": [ -6.31345772607836, 34.091198833058371 ] } },
+    { "type": "Feature", "properties": { "CODE": 6.0, "REGIONS": "Casablanca - Settat", "lon" : -7.7963242245599 , "lat" : 33.075024112733907 , "HOTSPOT": 19 }, "geometry": { "type": "Point", "coordinates": [ -7.7963242245599, 33.075024112733907 ] } },
+    { "type": "Feature", "properties": { "CODE": 7.0, "REGIONS": "Marrakech - Safi", "lon" : -8.310407925418486 , "lat" : 31.806125512229784 , "HOTSPOT": 9 }, "geometry": { "type": "Point", "coordinates": [ -8.310407925418486, 31.806125512229784 ] } },
+    { "type": "Feature", "properties": { "CODE": 5.0, "REGIONS": "Beni Mellal - Khenifra", "lon" : -6.298213354209073 , "lat" : 32.421477442451994 , "HOTSPOT": 16 }, "geometry": { "type": "Point", "coordinates": [ -6.298213354209073, 32.421477442451994 ] } },
+    { "type": "Feature", "properties": { "CODE": 3.0, "REGIONS": "Fès - Meknès", "lon" : -4.889724098081432 , "lat" : 33.739566712773211 , "HOTSPOT": 59 }, "geometry": { "type": "Point", "coordinates": [ -4.889724098081432, 33.739566712773211 ] } }
+    ]
+    };
